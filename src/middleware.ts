055dc0e33dbbc7768dd813cafa9b8f6e1584cbd8
Alexandre Morais <alexandremorais26@gmail.com>
2025-04-15 15:43:00 -0300

    change default route

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
   if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   return NextResponse.next();
